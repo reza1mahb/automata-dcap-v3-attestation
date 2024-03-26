6aede7a61abf26543843fcfa5d8ea24467771d73
reza1mahb <136892089+reza1mahb@users.noreply.github.com>
2024-03-26 08:42:46 +0330

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18"
+  solidity: "9.6.4"
 };
 
 export default config;
